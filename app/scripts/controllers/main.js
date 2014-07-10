069b3687f0983d6735da7119f4934642cbdc892b
Cliff Burger <cliffburger@daptiv.com>
2014-07-10 15:38:15 -0700

    Pump more data in and hide x axis labels

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,28 +1,30 @@
 'use strict';
 
 angular.module('chartBarApp')
-  .controller('MainCtrl', function ($scope) {
+    .controller('MainCtrl', function ($scope) {
         $scope.config = {
             tooltips: true
         };
 
         $scope.data = {
-            data: [{
-                x: "2014-1",
-                y: [100],
-                tooltip: "100 hours"
-            }, {
-                x: "2014-2",
-                y: [300],
-                tooltip: "300 hours"
-            }, {
-                x: "2014-3",
-                y: [351],
-                tooltip: "351 hours"
-            }, {
-                x: "2014-4",
-                y: [54],
-                tooltip: "54 hours"
-            }]
+            data: getChartData()
         };
-  });
+    });
+
+function getChartData() {
+    var data = [];
+    for (var i = 0; i < 100; i++) {
+        data.push(getData(i));
+    }
+
+    return data;
+}
+
+function getData(i) {
+    var hours = Math.round(Math.random() * 100);
+    return {
+        x: '2014-' + i,
+        y: [hours],
+        tooltip: hours + ' hours'
+    };
+}
